248e74a8caebc323ce31c0f368f999e92aef6a65
Damien <damien@dam.io>
2020-05-03 15:38:37 +0200

    add progress bar and group by output

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import itertools
+
 from django.views.generic import CreateView, DetailView
 
 from .tasks import launch_the_jobs
@@ -16,3 +18,18 @@ class HomeView(CreateView):
 
 class RunView(DetailView):
     model = Run
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        jobs = self.object.job_set.all()
+        finished_jobs = [job for job in jobs if job.finished]
+        context['finished'] = all([job.finished for job in jobs])
+        context['n_finished'] = len(finished_jobs)
+        context['n_jobs'] = len(jobs)
+        context['progress'] = context['n_finished'] / context['n_jobs'] * 100
+
+        sorted_jobs = sorted(finished_jobs, key=lambda job:job.output)
+        groups = itertools.groupby(sorted_jobs, key=lambda job:job.output)
+        context['outputs'] = [{'output':k, 'jobs': list(v)} for k, v in groups]
+        print(context['outputs'])
+        return context
